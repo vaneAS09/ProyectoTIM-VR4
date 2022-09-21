2e09a07e7911c0e5598e18b7ce570a238985b266
Vanessa Ayala Salas <84061086+vaneAS09@users.noreply.github.com>
2022-09-20 22:24:09 -0500

    Add files via upload

---
+++ fronttim/src/Resultados/Test.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import '../CSS/ContPDF.css'
+
+
+const Test = (props) => {
+    return(
+        <div className ='textPosition'>
+            <div className = 'textReporte'>
+            Siendo consecuentes con nuestra intención de acompañar la labor de orientación profesional 
+            que el colegio que usted representa tiene para con los estudiantes, a continuación presentamos 
+            el siguiente informe que tiene como objetivo compartir con ustedes la información 
+            obtenida de la actividad Inteligencias Múltiples realizada por los estudiantes de su institución, 
+            la cual refleja las preferencias que tienen en cuanto a programas académicos e inteligencias múltiples 
+            más relevantes.
+            <br/>
+            <br/>
+            El modelo de las Inteligencias Múltiples ha sido planteado por el conocido 
+            investigador en Educación, Howard Gardner, postulando que existen múltiples 
+            habilidades y caminos para lograr el éxito personal y profesional. 
+            El cuestionario de Orientación Vocacional según las Inteligencias Múltiples
+            ha sido diseñado para generar en los jóvenes una reflexión en torno a la relación de su 
+            perfil actual con los requerimientos de ingreso de las diferentes opciones académicas 
+            que puedan encontrar en la Educación Superior. Se clasifica como una prueba de Screening o 
+            sondeo, que permite acercarse a una sugerencia de programas académicos que se acerquen a la posible 
+            elección del estudiante, más no está pensado para dar una única respuesta precisa. 
+            Es un cuestionario auto-referenciado, es decir, depende de las opiniones directas de los estudiantes y 
+            no tiene características psicométricas, es decir, es una aproximación a lo que puedan ser las 
+            inteligencias múltiples de los jóvenes, más no es una medición técnica como tal. 
+            (Quiere decir: No estamos midiendo la inteligencia Lógico-Matemática, Lingüística, etc. Del estudiante, 
+            estamos aproximándonos basados en una auto-referencia).
+            <br/>
+            <br/>
+            Esta aplicación está dirigida a estudiantes de grados 9º, 10º y 11º y su 
+            utilización es práctica para abrir el tema de presentación de programas académicos, 
+            ya que vincula las experiencias presentes de los estudiantes con la proyección futura.
+            <br/>
+            <br/>
+            La identificación de sus principales Inteligencias Múltiples y su vínculo con las diferentes opciones 
+            académicas permiten al estudiante explorar de manera más precisa los posibles caminos 
+            académicos por los que podrá optar más adelante.
+            <br/>
+            <br/>
+            En nombre de la Universidad Autónoma de Occidente, le agradecemos el haber abierto las puertas de la institución educativa que usted representa y le reiteramos nuestra disposición para continuar realizando actividades 
+            complementarias que afiancen en los estudiantes y sus familias la decisión sobre el futuro profesional.
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            Esperamos que la información entregada sea de gran utilidad para ustedes y 
+            quedamos pendientes  de coordinar las actividades complementarias que deseen programar, 
+            para lo cual  se puede contactar con Juan Pablo Cortés - Profesional de Mercadeo Admisiones 
+            (Teléfono 3188000 Ext. 11199 - 11237; correo electrónico: mercadeoadmisiones@uao.edu.co - mercadeopregrado@uao.edu.co)
+            <br/>
+            <br/>
+            <br/>
+            Cordialmente.
+            <br/>
+            <br/>
+            <br/>
+            ___________________________________<br/>
+            María Fernanda Enríquez Rivera<br/>
+            Directora<br/>
+            Dirección de Mercadeo<br/>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            
+            <div className='titulos'> DATOS  IMPORTANTES  A TENER EN CUENTA </div>
+
+            <br/>
+            La Educación Superior en Colombia está dividida en ocho grandes áreas del conocimiento,  
+            que a su vez están divididos en 55 núcleos del conocimiento.
+            <br/>
+            Todos los programas ofrecidos por las universidades en Colombia pertenecen a uno de estos núcleos 
+            del conocimiento.
+            <br/>
+            Los programas académicos tienen ciertos requisitos de entrada que se 
+            pueden contrastar con los ocho tipos de inteligencias múltiples que existen:
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <br/><br/>
+            A continuación se relaciona la lista de carreras según áreas del conocimiento establecidas por el Ministerio de Educación nacional:
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            Convenciones de los resultados:
+            <br/>
+            <br/>
+            (Celda verde): Las celdas verdes indican que este tipo de inteligencia es 
+            especialmente importante para esta carrera.
+            <br/>
+            (Celda amarilla): Las celdas amarillas indican que este tipo de inteligencia es importante, 
+            más no indispensable para esta carrera. 
+            <br/>
+            <br/>
+            El desarrollo aproximado de cada una de las inteligencias se entiende en la siguiente escala: 
+            <br/>
+            <br/>
+            4-5: Alto nivel de desarrollo <br/>
+            3: Mediano nivel de desarrollo<br/>
+            0-2: Escaso nivel de desarrollo<br/>
+            <br/>
+            La idea es que las inteligencias múltiples que están en color VERDE tengan una mejor puntuación y las de color amarillo no sean menores a 3, 
+            para así  tener una mayor afinidad con la carrera escogida.
+            <br/>
+            <br/>
+            <br/>
+            Ficha técnica aplicación de pruebas múltiples
+            <br/>
+            </div>
+            
+        </div>
+    )
+}
+export default Test;
